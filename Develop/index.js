f2702e93b844bf6d1d8ed54b557677f02dd728ba
KevinKiely <Kevinkiely762@gmail.com>
2024-04-01 15:45:36 -0400

    Added all questions using inquirerer, all functions are working properly up to this point

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,101 @@
 // TODO: Include packages needed for this application
+// Import inquirer, fs the filing system, and generateMarkdown.js
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateReadMe = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    //Title
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    //License
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Select which license applicable to this project.",
+        choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD2", "BSD3", "Boost Software License 1.0", "Eclipse Public License 2.0", "Mozilla Public License 2.0", "N/A"],
+    },
+    //Description
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe the functionality of your project.",
+    },
+    //Screenshot (if applicable)
+    {
+        type: "input",
+        name: "screenshot",
+        message: "Please provide the path you would like to use as a screenshot, or input N/A.",
+    },
+    //Live application URL (if applicable)
+    {
+        type: "input",
+        name: "link",
+        message: "Please provide a URL where a user can access your deployed application, or input N/A.",
+    },
+    // Project Dependencies
+    {
+        type: "input",
+        name: "require",
+        message: "List any project dependencies here.",
+    },
+    // Usage
+    {
+        type: "input",
+        name: "usage",
+        message:
+            "Please list languages used in the development of this project.",
+    },
+    // Contributions
+    {
+        type: "input",
+        name: "contributors",
+        message: "Please list all contributors here.",
+        default: "",
+    },
+    // Testing
+    {
+        type: "input",
+        name: "testing",
+        message: "Provide a walkthrough of required tests if applicable.",
+    },
+
+    // Contact Information
+    {
+        type: "input",
+        name: "creator",
+        message: "Enter your github username if you wish to be contacted with questions regarding this project, otherwise leave blank.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email if you wish to be contacted with questions regarding this project, otherwise leave blank.",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log("Is this working?");
 
-// Function call to initialize app
+    });
+
+
+    // This is where you should take information from the questions array and add it to the empty README file
+
+}
+
+// Function call to initialize app, will get called when you do node index.js in command line
 init();
